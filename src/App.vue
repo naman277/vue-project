e5ec96ec13368b84949e2854ccb0888ccdb6c0e7
naman277 <153117899+naman277@users.noreply.github.com>
2026-04-01 15:26:14 +0530

    add basic functionalities like v-if v-else v-for v-on

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,47 @@
-<script setup></script>
+<script>
+  export default{
+    data(){
+      return {
+        name: 'John Doe',
+        status: 'Active',
+        tasks: ['Task 1', 'Task 2', 'Task 3'],
+        link: 'https://www.example.com',
+      };
+    },
+    methods:{
+      toggleStatus(){
+        if(this.status === "Active"){
+          this.status = "Pending";
+        }
+        else if(this.status === "Pending"){
+          this.status = "Inactive";
+        }
+        else{
+          this.status = "Active";
+        }
+      }
+    }
+  };
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Vue Jobs</h1>
+  <p>Hello, {{ name }}!</p>
+  <p v-if="status === 'Active'"> User is Active </p>
+  <p v-else-if="status === 'Pending'"> User is Pending </p>
+  <p v-else> User is Not Active </p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Click for example</a> -->
+  <a :href="link">Click here for example.com</a>
+  <br><br>
+  <button v-on:click="toggleStatus">Change Status</button>
 </template>
 
-<style scoped></style>
+<style>
+
+</style>
