e128d660fb270974d2293e8d062c2d0f4308ab7d
naman277 <153117899+naman277@users.noreply.github.com>
2026-04-20 16:02:30 +0530

    improve loader animation

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,59 +1,66 @@
 <script setup>
-import { reactive, defineProps, onMounted } from 'vue';
-import JobListing from './JobListing.vue';
-import { RouterLink } from 'vue-router';
-import { PulseLoader } from "vue-spinner"
-import axios from 'axios';
+import { reactive, defineProps, onMounted } from "vue";
+import JobListing from "./JobListing.vue";
+import { RouterLink } from "vue-router";
+import { PulseLoader } from "vue-spinner";
+import axios from "axios";
 
 const state = reactive({
-    jobs: [],
-    isLoading: true,
-})
+  jobs: [],
+  isLoading: true,
+});
 defineProps({
-    limit: Number,
-    showButton: {
-        type: Boolean,
-        default: false
-    }
-})
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 onMounted(async () => {
-    try{
-        const response = await axios.get('https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs');
-        state.jobs = response.data;
-    }catch(e){
-        console.log("Error fetching jobs: ",e);
-    }
-    finally{
-        state.isLoading = false;
-    }
-})
-
+  try {
+    const response = await axios.get(
+      "https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs",
+    );
+    state.jobs = response.data;
+  } catch (e) {
+    console.log("Error fetching jobs: ", e);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
-    <section class="bg-blue-50 px-4 py-10">
-        <div class="container-cl lg:container m-auto">
-            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-                Browse Jobs
-            </h2>
-            <!-- Show loading spinner while loading is true -->
-            <div v-if="state.isLoading" class="text-center text-gray-500 py-6"><PulseLoader /></div>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-cl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="loader-parent">
+        <div class="loader"></div>
+      </div>
 
-            <!-- Show job listing when data has been loaded -->
+      <!-- Show job listing when data has been loaded -->
 
-            <div v-else><div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in state.jobs.slice(0,limit || state.jobs.length)" :key="job.id" :job="job" />
-            </div></div>
+      <div v-else>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <JobListing
+            v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
+            :key="job.id"
+            :job="job"
+          />
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 
-    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <RouterLink
-            to="/jobs"
-            class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-            >View All Jobs</RouterLink
-        >
-    </section>
-    
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <RouterLink
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</RouterLink
+    >
+  </section>
 </template>
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { PulseLoader } from "vue-spinner"
-import {reactive, onMounted} from "vue"
-import { useRoute,useRouter, RouterLink } from "vue-router";
+import { PulseLoader } from "vue-spinner";
+import { reactive, onMounted } from "vue";
+import { useRoute, useRouter, RouterLink } from "vue-router";
 import { useToast } from "vue-toastification";
 import axios from "axios";
 import BackButton from "@/components/BackButton.vue";
@@ -12,93 +12,99 @@ const router = useRouter();
 const toast = useToast();
 const jobId = route.params.id;
 const state = reactive({
-    job: {},
-    isLoading: true,
-    notFound: false,
+  job: {},
+  isLoading: true,
+  notFound: false,
 });
 
 const deleteJob = async () => {
-    try {
-        const confirm = window.confirm('Are you sure you want to delete this job?');
-        if(confirm){
-            await axios.delete(`https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs/${jobId}`)
-            toast.success('Job deleted successfully')
-            router.push('/jobs')
-        }
-    } catch (error) {
-        console.error('Error deleting job: ',error);
-        toast.error("Job couldn't be deleted")
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+    if (confirm) {
+      await axios.delete(
+        `https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs/${jobId}`,
+      );
+      toast.success("Job deleted successfully");
+      router.push("/jobs");
     }
+  } catch (error) {
+    console.error("Error deleting job: ", error);
+    toast.error("Job couldn't be deleted");
+  }
 };
 
 onMounted(async () => {
-    try{
-        const response = await axios.get(`https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs/${jobId}`);
-        if (!response.data || !response.data.id) {
-            state.notFound = true;
-            return;
-        }
-        state.job = response.data;
-
-    }catch(e){
-        console.log("Error fetching job: ",e);
-        state.notFound = true;
+  try {
+    const response = await axios.get(
+      `https://69d4dad7d396bd74235dc85b.mockapi.io/api/jobs/${jobId}`,
+    );
+    if (!response.data || !response.data.id) {
+      state.notFound = true;
+      return;
     }
-    finally{
-        state.isLoading = false;
-    }
+    state.job = response.data;
+  } catch (e) {
+    console.log("Error fetching job: ", e);
+    state.notFound = true;
+  } finally {
+    state.isLoading = false;
+  }
 });
-
 </script>
 
 <template>
-    <BackButton />
-    <NotFoundView v-if="state.notFound" prevPage="jobs" material="job" Button="View Jobs" />
-    <div v-else-if="state.isLoading" class="text-center text-gray-500 py-6"><PulseLoader /></div>
-    <section v-else class="bg-green-50">
+  <BackButton />
+  <NotFoundView
+    v-if="state.notFound"
+    prevPage="jobs"
+    material="job"
+    Button="View Jobs"
+  />
+  <div v-else-if="state.isLoading" class="loader-parent">
+    <div class="loader"></div>
+  </div>
+  <section v-else class="bg-green-50">
     <div class="container m-auto py-10 px-6">
-        <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+      <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
         <main>
-            <div
+          <div
             class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
-            >
+          >
             <div class="text-gray-500 mb-4">{{ state.job.type }}</div>
             <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
             <div
-                class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+              class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
-                <i
-                class="pi pi-map-marker text-xl text-orange-700 mr-2"
-                ></i>
-                <p class="text-orange-700">{{ state.job.location }}</p>
+              <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
+              <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
-            </div>
+          </div>
 
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+          <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-green-800 text-lg font-bold mb-6">
-                Job Description
+              Job Description
             </h3>
 
             <p class="mb-4">
-                {{ state.job.description }}
+              {{ state.job.description }}
             </p>
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
 
             <p class="mb-4">{{ state.job.salary }} / Year</p>
-            </div>
+          </div>
         </main>
 
         <!-- Sidebar -->
         <aside>
-            <!-- Company Info -->
-            <div class="bg-white p-6 rounded-lg shadow-md">
+          <!-- Company Info -->
+          <div class="bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
             <h2 class="text-2xl">{{ state.job.company.name }}</h2>
 
             <p class="my-2">
-                {{ state.job.company.description }}
+              {{ state.job.company.description }}
             </p>
 
             <hr class="my-4" />
@@ -106,31 +112,65 @@ onMounted(async () => {
             <h3 class="text-xl">Contact Email:</h3>
 
             <p class="my-2 bg-green-100 p-2 font-bold">
-                {{ state.job.company.contactEmail }}
+              {{ state.job.company.contactEmail }}
             </p>
 
             <h3 class="text-xl">Contact Phone:</h3>
 
-            <p class="my-2 bg-green-100 p-2 font-bold">{{ state.job.company.contactPhone }}</p>
-            </div>
+            <p class="my-2 bg-green-100 p-2 font-bold">
+              {{ state.job.company.contactPhone }}
+            </p>
+          </div>
 
-            <!-- Manage -->
-            <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+          <!-- Manage -->
+          <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
             <RouterLink
-                :to="`/jobs/edit/${state.job.id}`"
-                class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</RouterLink
+              :to="`/jobs/edit/${state.job.id}`"
+              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+              >Edit Job</RouterLink
             >
-            <button @click="deleteJob"
-                class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+            <button
+              @click="deleteJob"
+              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
-                Delete Job
+              Delete Job
             </button>
-            </div>
+          </div>
         </aside>
-        </div>
+      </div>
     </div>
-    </section>
-
+  </section>
 </template>
+
+<style>
+/* HTML: <div class="loader"></div> */
+.loader-parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px; /* Adjust as needed */
+}
+
+.loader {
+  --d: 22px;
+  width: 4px;
+  height: 4px;
+  border-radius: 50%;
+  color: #084b09;
+  box-shadow:
+    calc(1 * var(--d)) calc(0 * var(--d)) 0 0,
+    calc(0.707 * var(--d)) calc(0.707 * var(--d)) 0 1px,
+    calc(0 * var(--d)) calc(1 * var(--d)) 0 2px,
+    calc(-0.707 * var(--d)) calc(0.707 * var(--d)) 0 3px,
+    calc(-1 * var(--d)) calc(0 * var(--d)) 0 4px,
+    calc(-0.707 * var(--d)) calc(-0.707 * var(--d)) 0 5px,
+    calc(0 * var(--d)) calc(-1 * var(--d)) 0 6px;
+  animation: l27 1s infinite steps(8);
+}
+@keyframes l27 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
